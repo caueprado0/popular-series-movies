f26f59dba03e02104dd45f71118e24ac68c670d6
Caue Prado <caue.prado0@gmail.com>
2021-03-08 14:43:28 -0300

    Adicionado mais headers.

--- src/services/api.js
+++ src/services/api.js
@@ -2,5 +2,10 @@ import axios from "axios";
 
 export default axios.create({
   baseURL: process.env.REACT_APP_MATERATE || "https://materate.com",
-  headers: { "Access-Control-Allow-Origin": "*" },
+  headers: {
+    "Access-Control-Allow-Origin": "*",
+    Accept: "application/json",
+    "Access-Control-Allow-Headers":
+      "Origin, X-Requested-With, Content-Type, Accept",
+  },
 });
