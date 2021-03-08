741e21335c7b6961c1ded4b9688038ac74401e7c
Caue Prado <caue.prado0@gmail.com>
2021-03-08 15:51:32 -0300

    Adicionado o link do netlify

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 # Popular Series Movies
 
+###Disponível em: https://popular-series-movies.netlify.app/
+
 Esse foi o meu primeiro projeto em React, totalmente sozinho, sem seguir alguma aula/tutorial. A ideia deste projeto é fazer uma lista de filmes e séries populares do momento.
 
 Me coloquei como objetivo usar apenas o React e os seus hooks, por se tratar de uma aplicação simples, não quis utilizar do Redux.
--- src/services/api.js
+++ src/services/api.js
@@ -3,13 +3,6 @@ import axios from "axios";
 export default axios.create({
   baseURL: process.env.REACT_APP_MATERATE || "https://materate.com",
   headers: {
-    "Access-Control-Allow-Origin": "*",
     Accept: "application/json",
-    "Access-Control-Allow-Credentials": true,
-    "Access-Control-Allow-Headers":
-      "Origin, X-Requested-With, Content-Type, Accept",
-    "x-requested-with": "XMLHttpRequest",
-    "sec-fetch-mode": "cors",
-    crossorigin: true,
   },
 });
